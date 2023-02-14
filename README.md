41f7cbec028fd0d11f8b97fb5de7aee03f87ab48
Giovanni Pietro Vitali <giovvi@gmail.com>
2023-02-14 12:47:20 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# world map
+
+The World Map locates 678 acts of translation of Jane Eyre, into 68 languages. By an ‘act of translation’ we mean either the first publication of a translation, or its re-publication in a new place: that is to say, we foreground the translations’ distribution across geography. This map presents the data in a more organized form than the General Map. The numbers you see on first encountering the World Map show how many acts of translation are recorded in a given area: as you zoom in they will divide. You can navigate and zoom using your mouse or touch-screen, and click on each point to reveal publication details.  
+
+The map was created by Giovanni Pietro Vitali, and represents research by the following collaborators in the project: Andrés Claro, Annmarie S. Drury, Mary Frank, Paola Gaudio, Rebecca Ruth Gould, Alessandro Grilli, Jernej Habjan, Yorimitsu Hashimoto, Yunte Huang, Adriana X. Jacobs, Eugenia Kelbert, Ulrich Timme Kragh, Abhishek Jain, Ida Klitgård, Léa Köves, Madli Kütt, Ana Teresa Marques dos Santos, Sowon S. Park, Cláudia Pazos-Alonso, Eleni Philippou, Yousif M. Qasmiyeh, Sasha Mile Rudan, Céline Sabiron, Emrah Serdan, Magda Szpindler, Kasia Szymanska, Kayvan Tahmasebian, Vincent Thiery, and Hongtao Wang, with assistance from Jay Dillon, Rachel Dryden, Karolina Gurevich, Chelsea Haith, Michael Reynolds, and Marta Ortega Sáez.  
+
+For discussion, see the project’s website –  https://prismaticjaneeyre.org/ – and its open access book: Prismatic Jane Eyre: Close-Reading a World Novel Across Languages: https://www.openbookpublishers.com/books/10.11647/obp.0319
+
+For details of the translations quoted, and discussion, see Matthew Reynolds et al., Prismatic Jane Eyre: Close-Reading a World Novel Across Languages (Cambridge: Open Book, 2023). https://doi.org/10.11647/OBP.0319
---
+++ script/01_general_script.R
@@ -0,0 +1,102 @@
+# This map is one of the results of the AHRC project Prismatic Jane Eyre.
+## It has been coded by Giovanni Pietro Vitali (Paris-Saclay University) with 
+## Matthew Reynolds (University of Oxford), who is the PI of the project.
+## Last update 5 December 2021
+
+# Call the libraries
+library(leaflet)
+library(sp)
+library(rgdal)
+library(RColorBrewer)
+library(leaflet.extras)
+library(leaflet.minicharts)
+library(htmlwidgets)
+library(raster)
+library(mapview)
+library(leafem)
+library(geojsonio)
+  
+  ## upload the data in Gojson and define the palette
+
+  countries <- geojsonio::geojson_read("data/geojson/countries.geojson", what = "sp")
+
+  pal <- colorBin("YlOrRd", domain = countries$annotation)
+  
+  ## upload the data in csv
+  data <- read.csv("data/csv/pje_2023_02.csv")
+  
+  ## Create the map  object
+  m <- leaflet(countries) %>% 
+  
+    ## Basemap
+    addTiles() %>%
+    ## Set the central view and zoom
+    setView(lng = 0.80, 
+            lat = 25.98, 
+            zoom = 1 ) %>%
+  
+    ## Add markers from the csv with pop-up
+    addMarkers(data = data, 
+               lng = ~lng, 
+               lat = ~lat,
+               group = "Languages",
+               popup = ~paste("<b>", label_place_of_publication, "</b>" ,"<br>", "<br>",
+                              "Language:", "<b>", language,"</b>","<br>",
+                              "Title:", "<b>", title, "</b>", "</b>","<br>",
+                              "Translator:", "<b>", translator_s, "</b>","<br>",
+                              "Date:", "<b>", date, "</b>", "<br>",
+                              "Publisher:", "<b>", publisher, "</b>", "<br>",
+                              "Country:", "<b>", country,"</b>","<br>",
+                              "Known reprints & re-editions in the same place:", "<b>", dates_of_reprints, "</b>", "<br>",
+                              "Notes:", "<b>", notes, "</b>", "<br>",
+                              sep = " "),
+               clusterOptions = markerClusterOptions()) %>%
+  
+    ## Add Polygon layer from the Geojson
+    addPolygons(data = countries,
+                fillColor = ~pal(countries$annotation),
+                weight = 0.1,
+                color = "brown",
+                dashArray = "3",
+                opacity = 0.7,
+                stroke = TRUE,
+                fillOpacity = 0.5,
+                smoothFactor = 0.5,
+                group = "Countries",
+                label = ~paste(titles, ": ", annotation, " act(s) of translation", sep = ""),
+                highlightOptions = highlightOptions(
+                  weight = 0.6,
+                  color = "#666",
+                  dashArray = "",
+                  fillOpacity = 0.7,
+                  bringToFront = TRUE))%>%
+    
+    ## Add a legend for the polygon layer based on its palette
+    addLegend("bottomleft", pal = pal, values = ~annotation,
+              title = "Acts of Translation",
+              labFormat = labelFormat(suffix = " per country"),
+              opacity = 1) %>%
+    
+   ## Add a legend for the map title
+    addLegend("topright",
+              colors = c("trasparent"),
+              labels=c("www.prismaticjaneeyre.org"),
+              title="Prismatic Jane Eyre") %>%
+    
+    ## Add a minimap to orient the user in zoom mode
+    addMiniMap() %>%
+    
+    ## Add a zoom reset button
+    addResetMapButton() %>%
+    
+    # Add the Layer control
+     addLayersControl(baseGroups = c("Language"),
+                      
+                      overlayGroups = c("Countries"),
+                      
+                      options = layersControlOptions(collapsed = TRUE))
+   
+  ## Run the map
+  m
+  
+  
